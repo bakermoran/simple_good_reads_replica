9219af690b872cbcdf8b60ce68148bc28f8c677e
Baker Moran <baker.moran@joinroot.com>
2020-09-26 11:52:25 -0400

    add news feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :books, only: [:index, :show]
     resources :authors, only: [:index, :show]
     resources :users, only: [:index, :show]
-    resources :reviews, only: [:show]
+    resources :reviews, only: [:index, :show]
     root 'welcome#index'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
